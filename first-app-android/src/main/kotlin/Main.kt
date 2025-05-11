908d4e7b38bc8742da98fa850cc34b2e1b22f7f0
Blarce <119196285+Blarce@users.noreply.github.com>
2025-05-11 23:15:21 +0300

    Add files via upload

---
+++ first-app-android/src/main/kotlin/Main.kt
@@ -0,0 +1,14 @@
+package org.example
+
+fun main() {
+    // User information
+    val name = "Шебанов Иван"
+    val age = 21
+    val hobby = "Разработчик"
+
+    // Displaying the information
+    println("Обо мне:")
+    println("Имя: $name")
+    println("Возраст: $age")
+    println("Профессия: $hobby")
+}
